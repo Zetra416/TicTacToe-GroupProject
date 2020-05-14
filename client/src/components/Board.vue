c2ca49f3a38832d74b3be68b86bf8f7634feeee3
chabode <rizqa.aulia@gmail.com>
2020-05-14 22:03:40 +0700

    layout

---
+++ client/src/components/Board.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="board">
+    <div v-for="(n,i) in 3" :key="i">
+      <div v-for="(n,j) in 3" :key="j">
+        <Cell :value="board[i][j]"> </Cell>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Cell from './Cell.vue';
+
+export default {
+  name: 'Board',
+  components: {
+    Cell,
+  },
+  data() {
+    return {
+      board: [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.board{
+  display: flex;
+  flex-wrap: wrap;
+  width: 204px;
+  height: 204px;
+}
+</style>
